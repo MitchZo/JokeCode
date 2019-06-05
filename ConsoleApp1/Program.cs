4ac935aabd1c66836e69d7b03678c9c1b8ee544c
Benjamin Donaldson <benjamindonaldson@quickenloans.com>
2019-06-05 13:57:54 -0400

    hellow world

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 
 namespace ConsoleApp1
 {
@@ -6,7 +7,22 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string destination = "hellow world";
+
+            string startingPoints = "            ";
+
+            byte[] asciiBytes = Encoding.ASCII.GetBytes(startingPoints);
+
+            for (int c = 0; c < destination.Length; c++)//see what I did there
+            {
+                while ((char)asciiBytes[c] != destination[c])
+                {
+                    asciiBytes[c]++;
+                    Console.WriteLine(Encoding.ASCII.GetString(asciiBytes, 0, asciiBytes.Length));
+                }
+
+            }
+
         }
     }
 }
