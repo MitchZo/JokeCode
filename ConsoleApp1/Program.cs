41538ad6ba695de187e22215ca9e6a6638d36333
Benjamin Donaldson <benjamindonaldson@quickenloans.com>
2019-06-07 11:36:04 -0400

    added a simple UI to our JokieBoi

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,11 +1,53 @@
 ﻿using System;
+using System.Reflection;
 using System.Text;
+
 
 namespace ConsoleApp1
 {
     class Program
     {
         static void Main(string[] args)
+        {
+            JokieBoi jokieBoi = new JokieBoi();
+            MethodInfo[] jokieBoisMethods = typeof(JokieBoi).GetMethods(BindingFlags.Static| BindingFlags.DeclaredOnly| BindingFlags.Public);        
+
+
+            GenerateMenu(jokieBoisMethods);
+            
+            int selection = -1;
+
+          
+            while(selection != jokieBoisMethods.Length + 1){
+                Console.WriteLine("Select Your Jokie Boi:");
+                string rawinput = Console.ReadLine().Trim();
+                int parsedInput = -1;
+
+                if(int.TryParse(rawinput, out parsedInput)){
+                    if((parsedInput - 1) < jokieBoisMethods.Length){
+                        jokieBoisMethods[parsedInput - 1].Invoke(jokieBoi, null);
+                        GenerateMenu(jokieBoisMethods);
+                    }
+                    selection = parsedInput;
+                }
+            }
+
+        }
+
+        static void GenerateMenu(MethodInfo[] jokieBoisMethods)
+        {
+            for(int i = 1; i <= jokieBoisMethods.Length; i++)
+            {
+                Console.WriteLine($"{i}){jokieBoisMethods[i-1].Name}");
+            }
+
+            Console.WriteLine($"{jokieBoisMethods.Length + 1})Quit");
+        }
+    }
+
+    public class JokieBoi
+    {
+        public static void SuperHelloWorld()
         {
             string destination = "hellow world";
 
@@ -22,7 +64,6 @@ namespace ConsoleApp1
                 }
 
             }
-
         }
     }
 }
