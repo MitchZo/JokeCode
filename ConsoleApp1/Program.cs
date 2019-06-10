6da62827940bdbfd4aa35e638d23b01a4559ac0e
Benjamin Donaldson <benjamindonaldson@quickenloans.com>
2019-06-10 11:45:18 -0400

    bad magician

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using System.Text;
-
+using System.Threading;
 
 namespace ConsoleApp1
 {
@@ -54,6 +54,16 @@ namespace ConsoleApp1
     //Any functions that you add to this JokieBoi will be included in the stupid little menu that the console prints out.
     public class JokieBoi
     {
+
+        public static void BreakDieRoller(){
+            for(int i = 0; i < 1000000; i++){
+                Console.WriteLine($"Attempt:{i}");
+                DiceRoll();
+            }
+
+            Console.WriteLine("You fool.  DiceRoller is unbreakable");
+        }
+
         public static void SuperHelloWorld()
         {
             string destination = "hellow world";
@@ -141,6 +151,75 @@ namespace ConsoleApp1
             Console.WriteLine("");
             Console.WriteLine("--------------");
             Console.WriteLine("");
+        }
+
+        public static void BadMagician(){
+            Console.WriteLine("Welcome to the Hairy Porters den of dark magicals.  I am Hairy Porter and I will use youre name alone to guess your phone number.");
+            var dontNeedThis = Console.ReadLine();
+
+
+            Console.WriteLine($"OOOOOOooooOOOOOhhh, {dontNeedThis}, what an interesting name.  Using this information, and only this information, I will guess your phone number");
+
+            List<string> fuckYous = new List<string>()
+            {
+                $"Wow {dontNeedThis}, arent you just a big fucking man, coming in here, and making me look like the asshole",
+                $"I bet you think you are so funny.",
+                $"You were adopted, and Im glad it wasnt by me",
+                $"Think again {dontNeedThis}, think again, because I learned this trick in prison",
+                $"*Weeps openly* *sighs* *collapses*",
+                $"Dont you fucking lie to me {dontNeedThis}.  Ive seen 'How To Train your dragon' 1-3 and I will nightfury your so hard",
+            };
+
+            List<string> sincereAppologies = new List<string>()
+            {
+                $"*deep breaths* alright alright, im sorry, its been a long week . {dontNeedThis}, please give me another shot",
+                $"Okay, that was a fluke.  Im a god dammed professional.  GATHER ROUND YOU FOOLS.  IM ABOUT TO BLOW THIS TINY MANS MIND",
+                $"*Slaps his own face* Okay, sorry, im low on magic juice.  Stand by *Pops flask*",
+                $"I will give you $700 to tweet that I got this right on the first try",
+                $"Here is your new sim card.  Abra Kadabra"
+            };
+
+            string phoneNumberGuess = GuessPhoneNumber();
+
+            Console.WriteLine($"Is this your phone number? - {phoneNumberGuess}");
+
+            var response = Console.ReadLine();
+
+            Random random = new Random();
+
+            while(!response.Equals("Yes", StringComparison.InvariantCultureIgnoreCase))
+            {
+
+                Console.WriteLine(fuckYous[random.Next(0, fuckYous.Count - 1)]);
+
+                int pregnantPausesCount = random.Next(3, 10);
+
+                for(int i = 0; i <= pregnantPausesCount; i++){
+                    Thread.Sleep(1000);
+                    Console.WriteLine("...");
+                }
+
+                
+                Console.WriteLine(sincereAppologies[random.Next(0, sincereAppologies.Count - 1)]);
+
+                phoneNumberGuess = GuessPhoneNumber();
+
+                Console.WriteLine($"Is this your phone number? - {phoneNumberGuess}");
+
+                response = Console.ReadLine();
+            }
+        }
+
+        private static string GuessPhoneNumber()
+        {
+
+            Random random = new Random();
+
+            int areaCode = random.Next(0, 999);
+            int demOtherThree = random.Next(0, 999);
+            int thoseFouryBois = random.Next(0, 9999);
+
+            return $"{String.Format("{0:D3}", areaCode)}-{String.Format("{0:D3}", demOtherThree)}-{String.Format("{0:D4}", thoseFouryBois)}";
         }
 
         public static void EnterBirthdate(){
